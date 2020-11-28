2c121b1e90850920f8dbf4a1fe482ab6b2ed9aa0
Samarsinh Salunkhe <smarsinh225@gmail.com>
2020-11-29 01:48:33 +0530

    basic app done

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-var username=prompt("your name")
+// var username=prompt("your name")
 
 
-var welcomeMessage="this script works "+username;
+// var welcomeMessage="this script works "+username;
 
-alert(welcomeMessage);
+// alert(welcomeMessage);
+
+
+var txtInput=document.querySelector("#txt-input");
+var btnTranslate=document.querySelector("#btn-translate");
+var outputDiv=document.querySelector("#output");
+
+
+
+console.log(outputDiv);
+
+function clickEventHandler(){
+    outputDiv.innerText="#$%#$" + txtInput.value;
+};
+
+
+btnTranslate.addEventListener("click",clickEventHandler)
+
+
+
+
+
+
+
